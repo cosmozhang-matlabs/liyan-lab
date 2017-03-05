274a4acd0762de54bdcd1f6694ac25cf4788bdab
Cosmo Zhang <cosmozhang1995@gmail.com>
2017-03-05 21:18:35 +0800

    dev

--- tree_rings/rescue_vs1/do_draw_seq_1.m
+++ tree_rings/rescue_vs1/do_draw_seq_1.m
@@ -3,27 +3,33 @@ function [  ] = do_draw_seq_1(  )
 load('wts_36_rawdata.mat');
 load('hs_20_rawdata.mat');
 
-% raw_years = wts_36_years;
-% raw_data = wts_36_rawdata;
-% name = 'Wutaishan';
+year_rngs = [1825, 1855; 1900, 1940; 1960, 2012];
+
+% for i = 1:size(year_rngs,1)
+for i = 1:1
+    
+    year_rng = year_rngs(i,:);
+    raw_years = wts_36_years;
+    raw_data = wts_36_rawdata;
+    name = 'Wutaishan';
+
+    datarng = 1:10;
+    draw_seq_1(raw_years, year_rng, raw_data, name, datarng);
+%     datarng = 11:20;
+%     draw_seq_1(raw_years, year_rng, raw_data, name, datarng);
+%     datarng = 21:30;
+%     draw_seq_1(raw_years, year_rng, raw_data, name, datarng);
+%     datarng = 31:36;
+%     draw_seq_1(raw_years, year_rng, raw_data, name, datarng);
 % 
-% datarng = 1:10;
-% draw_seq_1(raw_years, raw_data, name, datarng);
-% datarng = 11:20;
-% draw_seq_1(raw_years, raw_data, name, datarng);
-% datarng = 21:30;
-% draw_seq_1(raw_years, raw_data, name, datarng);
-% datarng = 31:36;
-% draw_seq_1(raw_years, raw_data, name, datarng);
-
-raw_years = hs_20_years;
-raw_data = hs_20_rawdata/1000;
-name = 'Hengshan';
-
-datarng = 1:10;
-draw_seq_1(raw_years, raw_data, name, datarng);
-datarng = 11:20;
-draw_seq_1(raw_years, raw_data, name, datarng);
+%     raw_years = hs_20_years;
+%     raw_data = hs_20_rawdata/1000;
+%     name = 'Hengshan';
+% 
+%     datarng = 1:10;
+%     draw_seq_1(raw_years, year_rng, raw_data, name, datarng);
+%     datarng = 11:20;
+%     draw_seq_1(raw_years, year_rng, raw_data, name, datarng);
 
 end
 
--- tree_rings/rescue_vs1/draw_seq_1.m
+++ tree_rings/rescue_vs1/draw_seq_1.m
@@ -1,16 +1,17 @@
-function [  ] = draw_seq_1( raw_years, raw_data, name, datarng )
+function [  ] = draw_seq_1( raw_years, year_rng, raw_data, name, datarng )
 
-years = [1825, 1855; 1900, 1940; 1960, 2012];
-year_rng = [1825 2012];
 year_span = year_rng(1):year_rng(end);
 
-data = NaN + zeros(size(raw_data));
-for i = 1:size(years, 1)
-    data(:, find(raw_years==years(i,1)):find(raw_years==years(i,end))) = raw_data(:, find(raw_years==years(i,1)):find(raw_years==years(i,end)));
-end
-data = data(datarng, find(raw_years==year_span(1)):find(raw_years==year_span(end)));
+% data = NaN + zeros(size(raw_data));
+% for i = 1:size(years, 1)
+%     data(:, find(raw_years==years(i,1)):find(raw_years==years(i,end))) = raw_data(:, find(raw_years==years(i,1)):find(raw_years==years(i,end)));
+% end
+% data = data(datarng, find(raw_years==year_span(1)):find(raw_years==year_span(end)));
 
-figure;
+data = raw_data(:, find(raw_years==year_rng(1)):find(raw_years==year_rng(end)));
+
+figure(1);
+set(gcf, 'Visible', 'off');
 base_pos = [0, 0, 18, 6 * size(data,1)];
 set(gcf, 'PaperPositionMode', 'manual');
 set(gcf, 'unit', 'inches');
@@ -18,16 +19,22 @@ set(gcf, 'position', base_pos);
 set(gcf, 'paperunits', 'inches');
 set(gcf, 'paperposition', base_pos);
 for i = 1:size(data,1)
+    data_to_plot = data(i,:);
     subplot(size(data,1),1,i);
-    plot(year_span, data(i,:));
-    set(gca, 'YLim', [0 5]);
+    plot(year_span, data_to_plot, 'LineWidth', 0.1, 'Color', [0.4 0.6 0.7]);
+    hold on;
+    plot(year_span, 1+zeros(size(year_span)), 'LineWidth', 0.1, 'Color', [0 0 0]+0.5, 'LineStyle', '--');
+    title(sprintf('%s(%d-%d) # %d', name, year_rng(1), year_rng(end), datarng(1)+i-1));
+    set(gca, 'YLim', [0, max(0.1, max(data_to_plot)*1.05)] );
     set(gca, 'YTick', [0, 1, 2, 3, 4, 5]);
-    set(gca, 'LineWidth', 0.1);
-    set(gca, 'FontSize', 6);
-    title(sprintf('%s # %d', name, datarng(1)+i-1));
+    set(gca, 'LineWidth', 1);
+    set(gca, 'FontSize', 1);
+    set(gca, 'FontUnit', 'points');
+%     set(gca, 'LabelFontSizeMultiplier', 1);
+%     set(gca, 'TitleFontSizeMultiplier', 1);
 end
 
-print(gcf, sprintf('%s %d-%d.tiff', name, datarng(1), datarng(end)), '-dtiff', '-r72');
+print(gcf, sprintf('%s(%d-%d) # %d-%d.tiff', name, year_rng(1), year_rng(end), datarng(1), datarng(end)), '-dtiff', '-r72');
 
 end
 
